17a2cde21a494a868259392c270377b0d848b9e0
Nikita Akimov <korchiy@yandex.ru>
2025-08-22 09:36:38 +0300

    update

--- mesh_split.py
+++ mesh_split.py
@@ -30,79 +30,76 @@ bl_info = {
 class MeshSplit:
 
     @classmethod
-    def split_by_density(cls, context, split_parts=5, split_mode='VERTS'):
+    def split_by_density(cls, context, split_parts=5, split_mode='VERTS', split_direction='X'):
         # split mesh by density
         #   mode 'VERTS' | 'FACES' - different counting modes
         if context.active_object and split_parts > 1:
             # execution time
+            first_time = time()
             start_time = time()
             # current mode
             mode = context.active_object.mode
-            # if context.active_object.mode == 'EDIT':
-            #     bpy.ops.object.mode_set(mode='OBJECT')
+            if context.active_object.mode == 'EDIT':
+                bpy.ops.object.mode_set(mode='OBJECT')
             # deselect all
             cls._deselect_all(obj=context.active_object)
-            print('after deselect all ', round(time() - start_time, 5))
+            print('after deselect all ', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+            start_time = time()
             # count number of vertices in one part
             vertices_in_a_part = round(cls.vertices_in_part(
                 context=context,
                 parts=split_parts
             ))
-            print('after vertices in a part', round(time() - start_time, 5))
+            print('after vertices in a part', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+            start_time = time()
             # split by split_parts
             if split_mode == 'VERTS':
                 print('VERTS mode')
                 # counting by vertices
-                for _i in range(split_parts - 1):   #  (-2 because ex: 5 parts = 3 cuts + 1 cut / 2)
-                    vertices_sorted = sorted(context.active_object.data.vertices, key=lambda _vert: _vert.co.x)
-                    print('after vertices_sorted', round(time() - start_time, 5))
+                for _i in range(split_parts - 1):   #  (-1 because ex: 5 parts = 4 cuts)
+                    if split_direction == 'X':
+                        vertices_sorted = sorted(context.active_object.data.vertices, key=lambda _vert: _vert.co.x)
+                    elif split_direction == 'Y':
+                        vertices_sorted = sorted(context.active_object.data.vertices, key=lambda _vert: _vert.co.y)
+                    else:   # 'Z'
+                        vertices_sorted = sorted(context.active_object.data.vertices, key=lambda _vert: _vert.co.z)
+                    print('after vertices_sorted', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
                     vertices_to_split = vertices_sorted[:vertices_in_a_part]
                     # we need to select faces by vertices_to_split
                     vertices_to_split_ids = set(_v.index for _v in vertices_to_split)
                     for face in context.active_object.data.polygons:
                         if set(face.vertices).issubset(vertices_to_split_ids):
                             face.select = True
-                    print('after face', round(time() - start_time, 5))
-                    context.active_object.data.validate()
-                    context.active_object.data.update()
-                    # context.active_object.update_from_editmode()
-
-                    # if selected faces exists - split
-                    selected_faces_n = len([_face for _face in context.active_object.data.polygons if _face.select])
-                    print('selected_faces_n', selected_faces_n)
-                    if selected_faces_n:
-                        # bpy.ops.object.mode_set(mode='EDIT')
-                        print('after edit', round(time() - start_time, 5))
-                        bpy.ops.mesh.separate(type='SELECTED')
-                        print('after split', round(time() - start_time, 5))
-                        # bpy.ops.object.mode_set(mode='OBJECT')
-                        print('after object', round(time() - start_time, 5))
-                    print('_i', _i)
-                # if in the last part vertices amount > vertices_in_a_part - split it into 2 parts by Y
-                remaining_vertices_n = len(context.active_object.data.vertices)
-                if remaining_vertices_n >= vertices_in_a_part:
-                    vertices_sorted = sorted(context.active_object.data.vertices, key=lambda _vert: _vert.co.y)
-                    vertices_to_split = vertices_sorted[:vertices_in_a_part]
-                    # we need to select faces by vertices_to_split
-                    vertices_to_split_ids = set(_v.index for _v in vertices_to_split)
-                    for face in context.active_object.data.polygons:
-                        if set(face.vertices).issubset(vertices_to_split_ids):
-                            face.select = True
+                    print('after face', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
+                    # context.active_object.data.validate()
+                    # context.active_object.data.update()
+                    # # context.active_object.update_from_editmode()
                     # if selected faces exists - split
                     selected_faces_n = len([_face for _face in context.active_object.data.polygons if _face.select])
                     if selected_faces_n:
                         bpy.ops.object.mode_set(mode='EDIT')
+                        print('after switch to edit mode', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                        start_time = time()
                         bpy.ops.mesh.separate(type='SELECTED')
+                        print('after split', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                        start_time = time()
                         bpy.ops.object.mode_set(mode='OBJECT')
+                        print('after switch to object mode', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                        start_time = time()
+                    print('end of iteration', _i)
             elif split_mode == 'FACES':
                 print('FACES mode')
                 # counting by faces centroids
-                for _i in range(split_parts - 1):   #  (-2 because ex: 5 parts = 3 cuts + 1 cut / 2)
+                for _i in range(split_parts - 1):   #  (-1 because ex: 5 parts = 4 cuts)
                     # get faces sorted by centroids by local X coordinate
                     faces_sorted = cls._faces_sorted_by_centroid(
                         obj=context.active_object,
-                        axis='X'
+                        axis=split_direction,
                     )
+                    print('after faces_sorted', round(time() - first_time, 5), '(+', round(time() - start_time, 5),')')
+                    start_time = time()
                     # we need to get faces from faces_sorted with summar number of vertices near number of vertices_in_a_part
                     #   and split it from current object
                     faces_vertices = set()  # vertices counter
@@ -115,44 +112,78 @@ class MeshSplit:
                         if len(faces_vertices) < vertices_in_a_part:
                             _face[0].select = True
                         else:
+                            print('after faces selected', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                            start_time = time()
                             # if selected faces exists - split
                             selected_faces_n = len([_face for _face in context.active_object.data.polygons if _face.select])
                             if selected_faces_n:
                                 bpy.ops.object.mode_set(mode='EDIT')
+                                print('after switch to edit mode', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                                start_time = time()
                                 bpy.ops.mesh.separate(type='SELECTED')
+                                print('after split', round(time() - first_time, 5), '(+', round(time() - start_time, 5),')')
+                                start_time = time()
                                 bpy.ops.object.mode_set(mode='OBJECT')
+                                print('after switch to object mode', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                                start_time = time()
                                 # after splitting we need to start from beginning because mesh was rebuilt
                                 break
-                # if in the last part vertices amount > vertices_in_a_part - split it into 2 parts by Y
-                remaining_vertices_n = len(context.active_object.data.vertices)
-                if remaining_vertices_n >= vertices_in_a_part:
-                    # get faces sorted by centroids by local X coordinate
-                    faces_sorted = cls._faces_sorted_by_centroid(
-                        obj=context.active_object,
-                        axis='Y'
-                    )
-                    faces_vertices = set()  # vertices counter
-                    # walk from left by sorted faces
-                    for _face in faces_sorted:
-                        # increase counter by faces vertices amount
-                        faces_vertices.update(_face[1])
-                        # if counter less vertices_in_a_part - select face and continue#
-                        # else - split selected faces
-                        if len(faces_vertices) < round(remaining_vertices_n / 2):
-                            _face[0].select = True
-                        else:
-                            # if selected faces exists - split
-                            selected_faces_n = len([_face for _face in context.active_object.data.polygons if _face.select])
-                            if selected_faces_n:
-                                bpy.ops.object.mode_set(mode='EDIT')
-                                bpy.ops.mesh.separate(type='SELECTED')
-                                bpy.ops.object.mode_set(mode='OBJECT')
-                                # after splitting we need to start from beginning because mesh was rebuilt
-                                break
+                    print('end of iteration', _i)
+            elif split_mode == 'BM':
+                print('BM mode')
+                # use bmesh
+                bm = bmesh.new()
+                bm.from_mesh(context.active_object.data)
+                bm.verts.ensure_lookup_table()
+                bm.faces.ensure_lookup_table()
+                # splitting to number of parts
+                for _i in range(split_parts - 1):  # (-1 because ex: 5 parts = 4 cuts + 1 cut / 2)
+                    if split_direction == 'X':
+                        vertices_sorted = sorted(bm.verts, key=lambda _vert: _vert.co.x)
+                    elif split_direction == 'Y':
+                        vertices_sorted = sorted(bm.verts, key=lambda _vert: _vert.co.y)
+                    else:   # 'Z'
+                        vertices_sorted = sorted(bm.verts, key=lambda _vert: _vert.co.z)
+                    print('after vertices_sorted', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
+                    vertices_to_split = set(vertices_sorted[:vertices_in_a_part])
+                    # we need to select faces by vertices_to_split
+                    for face in bm.faces:
+                        if set(face.verts).issubset(vertices_to_split):
+                            face.select = True
+                    print('after face selected', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
+                    # from selected faces we need to create data for from_pydata function (faces and vertices_co lists)
+                    selected_faces = [face for face in bm.faces if face.select]
+                    vertices = list(set(v for f in selected_faces for v in f.verts))
+                    coordinates = [v.co for v in vertices]
+                    vmap = dict()
+                    for i, vert in enumerate(vertices):
+                        vmap[vert.index] = i
+                    faces = []
+                    for face in selected_faces:
+                        faces.append([vmap[v.index] for v in face.verts])
+                    print('after preparing pydata', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
+                    # remove selected faces not to use them on the next step
+                    bmesh.ops.delete(bm, geom=selected_faces, context=5)
+                    print('after remove', round(time() - first_time, 5), '(+', round(time() - start_time, 5), ')')
+                    start_time = time()
+                    # create new mesh from collected data
+                    me = bpy.data.meshes.new(name=context.active_object.name)
+                    me.from_pydata(coordinates, [], faces)
+                    scene = context.scene
+                    obj = bpy.data.objects.new(context.active_object.name, me)
+                    scene.objects.link(obj)
+                    obj.matrix_world = context.active_object.matrix_world.copy()
+                    print('end of iteration', _i)
+                # save changed data to mesh
+                bm.to_mesh(context.active_object.data)
+                bm.free()
             # return mode back
             bpy.ops.object.mode_set(mode=mode)
             # execution time
-            print('Executed in %s seconds' % (round(time() - start_time, 2)))
+            print('Executed in %s seconds' % (round(time() - first_time, 2)))
 
     @staticmethod
     def vertices_in_part(context, parts):
@@ -212,6 +243,7 @@ class MeshSplit:
         )
         op.split_parts = context.window_manager.mesh_split_1d_prop_split_parts
         op.split_mode = context.window_manager.mesh_split_1d_prop_split_mode
+        op.split_direction = context.window_manager.mesh_split_1d_prop_split_direction
         layout.prop(
             data=context.window_manager,
             property='mesh_split_1d_prop_split_parts'
@@ -219,6 +251,11 @@ class MeshSplit:
         layout.prop(
             data=context.window_manager,
             property='mesh_split_1d_prop_split_mode',
+            expand=True
+        )
+        layout.prop(
+            data=context.window_manager,
+            property='mesh_split_1d_prop_split_direction',
             expand=True
         )
         layout.label(
@@ -245,15 +282,26 @@ class MeshSplit_OT_split_by_density(Operator):
         items=[
             ('VERTS', 'VERTS', 'VERTS', '', 0),
             ('FACES', 'FACES', 'FACES', '', 1),
+            ('BM', 'BM', 'BM', '', 2)
         ],
         default='VERTS'
+    )
+    split_direction = EnumProperty(
+        name='Split Direction',
+        items=[
+            ('X', 'X', 'X', '', 0),
+            ('Y', 'Y', 'Y', '', 1),
+            ('Z', 'Z', 'Z', '', 2)
+        ],
+        default='X'
     )
 
     def execute(self, context):
         MeshSplit.split_by_density(
             context=context,
             split_parts=self.split_parts,
-            split_mode=self.split_mode
+            split_mode=self.split_mode,
+            split_direction=self.split_direction
         )
         return {'FINISHED'}
 
@@ -286,8 +334,18 @@ def register(ui=True):
         items=[
             ('VERTS', 'VERTS', 'VERTS', '', 0),
             ('FACES', 'FACES', 'FACES', '', 1),
+            ('BM', 'BM', 'BM', '', 2)
         ],
         default='VERTS'
+    )
+    WindowManager.mesh_split_1d_prop_split_direction = EnumProperty(
+        name='Split Direction',
+        items=[
+            ('X', 'X', 'X', '', 0),
+            ('Y', 'Y', 'Y', '', 1),
+            ('Z', 'Z', 'Z', '', 2)
+        ],
+        default='X'
     )
 
     register_class(MeshSplit_OT_split_by_density)
@@ -300,6 +358,8 @@ def unregister(ui=True):
         unregister_class(MeshSplit_PT_panel)
     # butch clean
     unregister_class(MeshSplit_OT_split_by_density)
+    del WindowManager.mesh_split_1d_prop_split_direction
+    del WindowManager.mesh_split_1d_prop_split_mode
     del WindowManager.mesh_split_1d_prop_split_parts
 
 
